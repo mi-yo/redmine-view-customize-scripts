e6245c722c39e3f80a021a12816f6d21ae9d21bb
mi-yo <miyamoto.yoichi@gmail.com>
2016-03-04 11:27:30 +0900

    Apply previous month to "Apply date to gantt menu link"

--- script/apply_date_to_gantt_menu_link.js
+++ script/apply_date_to_gantt_menu_link.js
@@ -4,7 +4,9 @@ Type: JavaScript
 Apply date to gantt menu link.
 */
 $(function() {
-  $('#main-menu > ul').find('li .gantt').attr('href',function($){
-      return this+'?months=2&month=3&year=2016&zoom=4';
+  $('#main-menu > ul').find('li .gantt').attr('href', function(){
+    // Apply previous month
+    var date = new Date();
+    return this + '?months=2&month=' + date.getMonth() + '&year=' + date.getFullYear() + '&zoom=4';
   });
 });
